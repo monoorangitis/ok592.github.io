7e88b59720e8812d62a23ca26b1483d0c1e59f84
Elias Marold <e1425778@student.tuwien.ac.at>
2018-04-03 22:22:20 +0200

    Interactivity added, static mode added, choicebox added

--- parallelCoordinates.js
+++ parallelCoordinates.js
@@ -30,7 +30,6 @@ d3.csv("infoVis.csv", function (csvData) {
     dimensions = d3.keys(data[0]);
     dimensions.splice(0, 1);
 
-
     //-------------------------------------------- parallel coordinates ----------------------------------------------//
 
 
@@ -60,6 +59,17 @@ d3.csv("infoVis.csv", function (csvData) {
         }));
     }
 
+    // Init Menu
+    initMenu(["Static", "Interactive"]);
+
+    i = 0;
+    function google_colors(d) {
+        i += 1;
+        var google_cols = ["#3366cc", "#dc3912", "#ff9900", "#109618", "#990099", "#0099c6", "#dd4477", "#66aa00", "#b82e2e", "#316395", "#994499", "#22aa99", "#aaaa11", "#6633cc", "#e67300", "#8b0707", "#651067", "#329262", "#5574a6", "#3b3eac"];
+        return google_cols[d % google_cols.length];
+    }
+
+    oldColor = null;
     // render parallel coordinates polylines and store them back
     polylines = svgPC.append("g")
         .attr("class", "polyline") // see {@link stylesheet.css} for details
@@ -67,30 +77,36 @@ d3.csv("infoVis.csv", function (csvData) {
         .data(data)
         .enter().append("path")
         .attr("d", path)
+        .style("stroke-width", "2")
+        .style("stroke", function() {
+            return google_colors(i)})
         // add tooltip action!
         .on("mouseover", function (d) {
-            highlighted = d; // store back highlighted element
+            if(readMenu()==="Interactive"){
 
-            d3.select(this).style("stroke", "lightsteelblue"); // highlight line: color it and increase the stroke-width
-            d3.select(this).style("stroke-width", "5");
+                highlighted = d; // store back highlighted element
+                oldColor = d3.select(this).style("stroke"); // store back highlighted element's color
+                d3.select(this).style("stroke", "lightsteelblue"); // highlight line: color it and increase the stroke-width
+                d3.select(this).style("stroke-width", "5");
 
-            tooltip = d3.select("body").append("div") // set the div, see {@link stylesheet.css} for details
-                .attr("id", "tooltip")
-                .attr("class", "tooltip");
-            tooltip.html(d.name || d.Name || d.species || d.ID) // show the name on mouseover (accounts for all naming-schemes), add x and y offset
-                .style("left", (d3.event.pageX) + "px")
-                .style("top", (d3.event.pageY - 10) + "px");
-
-            redrawHighlightedCircle(); // call linked highlighting-method
+                tooltip = d3.select("body").append("div") // set the div, see {@link stylesheet.css} for details
+                    .attr("id", "tooltip")
+                    .attr("class", "tooltip");
+                tooltip.html(d.Name) // show the name on mouseover (accounts for all naming-schemes), add x and y offset
+                    .style("left", (d3.event.pageX) + "px")
+                    .style("top", (d3.event.pageY - 10) + "px");
+            }
         })
         .on("mouseout", function (d) {
-            highlighted = null;
+            if(readMenu()==="Interactive") {
+                highlighted = null;
 
-            d3.select("#tooltip").remove();
-            d3.select(this).style("stroke", "black"); // undo highlighting (reset line color)
-            d3.select(this).style("stroke-width", "1"); // highlight line
+                d3.select("#tooltip").remove();
+                d3.select(this).style("stroke", oldColor); // undo highlighting (reset line color)
+                d3.select(this).style("stroke-width", "1"); // highlight line
 
-            redrawHighlightedCircle();
+                oldColor = null;
+            }
         });
 
     // parallel coordiantes axes container
@@ -113,4 +129,66 @@ d3.csv("infoVis.csv", function (csvData) {
         .style("text-anchor", "middle")
         .attr("y", margin.top / 2)
         .text(String); // Get domain name from data
+
+
+
+    i = 0;
+    j = 0;
+    function pos(d) {
+        j += 1;
+        var heights = [40, 80, 120, 160, 200, 240, 280, 320, 360, 400, 440, 480, 520];
+        return (heights[d % heights.length]) + "px";
+    }
+
+    names_divs = d3.select("#names_div")
+        .selectAll("div")
+        .data(data).enter()
+        .append("div")
+        .style("width", "100px")
+        .style("height", "30px")
+        .style("top", function() {
+            return pos(j)})
+        .style("background",function() {return google_colors(i);})
+        .attr("class", "names")
+        .html(function (d) { return d.Name;});
+
+    function render() {
+
+        if(readMenu() === ("Interactive")){
+            // remove descriptions
+            d3.select("#names_div").style("opacity", "0");
+
+            polylines.style("stroke", "black").style("stroke-width", "1");
+
+        } else if(readMenu() === ("Static")){
+            // add descriptions
+            d3.select("#names_div").style("opacity", "1");
+
+            i = 0;
+            polylines.style("stroke-width", "2")
+                .style("stroke", function() {
+                return google_colors(i)});
+        }
+
+    }
+
+    //------------------------------------------------------ menu ----------------------------------------------------//
+
+    // init menu
+    function initMenu(entries){
+        entries.forEach(function(d) {
+            $( "select#Mode").append("<option>"+d+"</option>");
+        });
+
+        $( "#Mode" ).selectmenu({
+            select: function() {
+                render();
+            }
+        });
+    }
+
+    // read current scatterplot parameters
+    function readMenu(){
+        return $( "#Mode").val();
+    }
 });
